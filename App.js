cce7aa5323c2445ec2d87092709eb4ad4f9aa2be
tyogautomo <yoga.utomo27@gmail.com>
2020-02-07 09:42:14 +0700

    pagi guys

--- App.js
+++ App.js
@@ -6,109 +6,74 @@
  * @flow
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Component } from 'react';
+import { View, StatusBar, Text, Button, Image, ScrollView } from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+  state = {
+    profilePicturePath: '',
+    profilePictureMime: '',
+    profileBackgroundPath: '',
+    profileBackgroundMime: ''
+  }
+
+  onPressProfilePicture = async () => {
+    try {
+      const image = await ImagePicker.openPicker({
+        width: 500,
+        height: 500,
+        cropping: true
+      })
+
+      this.setState({
+        profilePicturePath: image.path,
+        profilePictureMime: image.mime
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  onPressProfileBackground = async () => {
+    try {
+      const image = await ImagePicker.openPicker({
+        width: 500,
+        height: 500,
+        cropping: true
+      })
+
+      this.setState({
+        profileBackgroundPath: image.path,
+        profileBackgroundMime: image.mime
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  render() {
+    const { profilePicturePath, profileBackgroundPath } = this.state;
+    return (
+      <ScrollView>
+        <StatusBar barStyle="dark-content" />
+        <View>
+          <Text style={{ textAlign: 'center', marginBottom: 40, fontSize: 40 }}>Multiple Upload example</Text>
+          <View style={{ alignItems: 'center' }}>
+            {profilePicturePath ? <Image source={{ uri: this.state.profilePicturePath }} style={{ width: 200, height: 200, marginBottom: 20 }} /> : null}
+            <View style={{ marginBottom: 20 }}>
+              <Button title="Pick Profile Picture" onPress={this.onPressProfilePicture} />
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
+            {profileBackgroundPath ? <Image source={{ uri: this.state.profileBackgroundPath }} style={{ width: 200, height: 200, marginBottom: 20 }} /> : null}
+            <View style={{ marginBottom: 20 }}>
+              <Button title="Pick Profile Background" onPress={this.onPressProfileBackground} />
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+        </View>
+      </ScrollView>
+    );
+  }
+}
 
 export default App;
